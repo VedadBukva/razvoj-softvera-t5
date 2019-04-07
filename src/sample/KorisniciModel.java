ec6d79919f5ea2cda7f77ab593b23987170937b2
Vedad Bukva <vedadbukva8@gmail.com>
2019-04-07 22:51:58 +0200

    atomic commit

---
+++ src/sample/KorisniciModel.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.beans.property.SimpleObjectProperty;
+
+import javafx.collections.FXCollections;
+
+import javafx.collections.ObservableList;
+
+public class KorisniciModel {
+    private ObservableList<Korisnik> korisnik = FXCollections.observableArrayList();
+    private SimpleObjectProperty trenutniKorisnik = new SimpleObjectProperty();
+
+    public ObservableList<Korisnik> getKorisnik() {
+        return korisnik;
+    }
+
+    public void setKorisnik(ObservableList<Korisnik> korisnik) {
+        this.korisnik = korisnik;
+    }
+
+    public Object getTrenutniKorisnik() {
+        return trenutniKorisnik.get();
+    }
+
+    public SimpleObjectProperty trenutniKorisnikProperty() {
+        return trenutniKorisnik;
+    }
+
+    public void setTrenutniKorisnik(Object trenutniKorisnik) {
+        this.trenutniKorisnik.set(trenutniKorisnik);
+    }
+
+    void napuni (){
+        korisnik.add(new Korisnik( "Vedad" , "Bukva" , " vbukva1@etf.unsa.ba" , "vbukva1", "password"));
+        korisnik.add(new Korisnik( "Tarik" , "Sijarcic" , " tsijarcic1@etf.unsa.ba" , "tare", "passw"));
+    }
+
+}
